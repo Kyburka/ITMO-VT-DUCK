53cef75cbd7b4eb7b453a866f5630b569ce9042d
Kyburka <xkyburkax@mail.ru>
2025-10-20 12:55:04 +0300

    fix task2.sh

--- Csbasics/Labwork 1/task2.sh
+++ Csbasics/Labwork 1/task2.sh
@@ -3,22 +3,22 @@ cd braviary5
 chmod u=r,g=r,o= herdier
 chmod u=rw,g=r,o=r combee
 chmod 004 vibrava
-chmod u=rw, stunky
+chmod u=rw,g=w,o=r stunky
 cd ~/lab0
 chmod 711 braviary5
-chmod 044 chingling0
+chmod u=,g=r,o=r chingling0
 chmod 006 cottonee1
-chmod 444 doduo5
+chmod u=r,g=r,o=r doduo5
 cd oshawott9
 chmod 337 beheeyem
-chmod 404 slowpoke
+chmod u=r,g=,o=r slowpoke
 chmod 640 corphish
-chmod 006 riolu
-chmod 062 venusaur
+chmod u=,g=,o=rw riolu
+chmod u=,g=rw,o=w venusaur
 cd ~/lab0/teddiursa1
-chmod 311 klingklang
+chmod u=wx,g=x,o=x klingklang
 chmod 305 murkrow
-chmod 664 poliwrath
+chmod u=rw,g=rw,o=r poliwrath
 cd ~/lab0/
 chmod 751 teddiursa1
 chmod 750 oshawott9
